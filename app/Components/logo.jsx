6f28042a792cb78f38c445149980762cbce6b43d
gonzalm96 <mgonzalm96@gmail.com>
2025-07-11 10:19:37 -0700

    making the logo a link back to home

--- app/Components/logo.jsx
+++ app/Components/logo.jsx
@@ -4,7 +4,9 @@ import LogoFile from "../../public/images/Fuzzy_Britches_Farm_Logo_Pass_2.png";
 export default function Logo(){
     return(
         <div>
-            <Image src={LogoFile} height="101" width="131" alt="Fuzzy Britches Fiber" />
+            <a href="/">
+                <Image src={LogoFile} height="101" width="131" alt="Fuzzy Britches Fiber" />
+            </a>
         </div>
     )
 }
