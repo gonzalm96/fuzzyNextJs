445ae2c7eb676c910a3637221c1721b23b657214
gonzalm96 <mgonzalm96@gmail.com>
2025-07-30 10:59:37 -0700

    adding simple mobile nav

--- app/Components/navBar.jsx
+++ app/Components/navBar.jsx
@@ -33,47 +33,44 @@ export default function NavBar(){
 
     function DesktopNav() {
         return(
-            <ul className={navStyle.navListDesktop}>
-                <li className={signika.className + " " + navStyle.navItem}><Link href="/">Home</Link></li> 
-                <li className={signika.className + " " + navStyle.navItem}><Link href="/about">About Us</Link></li>
-                <li className={signika.className + " " + navStyle.navItem}><a href="https://docs.google.com/document/d/17tlhusqXq7PFRhfEtKDbxMnCbZxt8IMC346OVQLwqJI/edit?usp=sharing"> Local Resources </a></li>
-                <li className={signika.className + " " + navStyle.navItem}><Link href="/contact">Contact</Link></li>
-                <li className={signika.className + " " + navStyle.navItem}><CtaButton primary={true} nav={true} link={"https://www.etsy.com/shop/fuzzybritchesfiber/?etsrc=sdt"} linkText={"Visit Our Shop!"}/></li>
-            </ul>
+            <div className={navStyle.navContainer + " " + navStyle.desktop}>
+                <Logo />
+                <ul className={navStyle.navListDesktop}>
+                    <li className={signika.className + " " + navStyle.navItem}><Link href="/">Home</Link></li> 
+                    <li className={signika.className + " " + navStyle.navItem}><Link href="/about">About Us</Link></li>
+                    <li className={signika.className + " " + navStyle.navItem}><a href="https://docs.google.com/document/d/17tlhusqXq7PFRhfEtKDbxMnCbZxt8IMC346OVQLwqJI/edit?usp=sharing"> Local Resources </a></li>
+                    <li className={signika.className + " " + navStyle.navItem}><Link href="/contact">Contact</Link></li>
+                    <li className={signika.className + " " + navStyle.navItem}><CtaButton primary={true} nav={true} link={"https://www.etsy.com/shop/fuzzybritchesfiber/?etsrc=sdt"} linkText={"Visit Our Shop!"}/></li>
+                </ul>
+            </div>
         )
     }
 
     function MobileNav() {
         return(
-            <div className={navStyle.navList}>
-                <button className={navStyle.mobileMenuBtn} onClick={activateMobileNav}>
-                    <Image src={HamburgerMenu} height={40} width={40} alt="Open Navigation Menu"/>
-                </button>
-                <div className={slideOut ? navStyle.showNav:navStyle.hideNav}>
-                    <ul className={navStyle.navListMobile}>
-                        <li>
-                            <button className={navStyle.mobileMenuBtn} onClick={activateMobileNav}>
-                                <Image src={HamburgerMenu} height={40} width={40} alt="Open Navigation Menu"/>
-                            </button>
-                        </li>
-                        <li className={signika.className + " " + navStyle.navItem}><Link href="/">Home</Link></li> 
-                        <li className={signika.className + " " + navStyle.navItem}><Link href="/about">About Us</Link></li>
-                        <li className={signika.className + " " + navStyle.navItem}><a href="https://docs.google.com/document/d/17tlhusqXq7PFRhfEtKDbxMnCbZxt8IMC346OVQLwqJI/edit?usp=sharing"> Local Resources </a></li>
-                        <li className={signika.className + " " + navStyle.navItem}><Link href="/contact">Contact</Link></li>
-                        <li className={signika.className + " " + navStyle.navItem}><CtaButton primary={true} nav={true} link={"https://www.etsy.com/shop/fuzzybritchesfiber/?etsrc=sdt"} linkText={"Visit Our Shop!"}/></li>
-                    </ul>
+            <div className={navStyle.navContainer + " " + navStyle.mobile}>
+                <div className={navStyle.mobileHeading}>
+                    <Logo />
+                    <CtaButton primary={true} nav={true} link={"https://www.etsy.com/shop/fuzzybritchesfiber/?etsrc=sdt"} linkText={"Visit Our Shop!"}/>
+                </div>
+                <div className={navStyle.navList}>
+                    <div className={slideOut ? navStyle.showNav:navStyle.hideNav}>
+                        <ul className={navStyle.navListMobile}>
+                            <li className={signika.className + " " + navStyle.navItem}><Link href="/">Home</Link></li> 
+                            <li className={signika.className + " " + navStyle.navItem}><Link href="/about">About Us</Link></li>
+                            <li className={signika.className + " " + navStyle.navItem}><a href="https://docs.google.com/document/d/17tlhusqXq7PFRhfEtKDbxMnCbZxt8IMC346OVQLwqJI/edit?usp=sharing"> Resources </a></li>
+                            <li className={signika.className + " " + navStyle.navItem}><Link href="/contact">Contact</Link></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
     }
 
     return(
-        <div className={navStyle.nav}>
-            <div className={navStyle.navContainer}>
-            <Logo />
+        <div className={navStyle.nav}>  
             <DesktopNav />
             <MobileNav />
-            </div>
         </div>
     )
 }
